818324f5bfea806593b2ff2a3c5dfbc8d560ebaa
jeffreyreagan <116695597+jeffreyreagan@users.noreply.github.com>
2024-01-15 12:17:54 -0500

    debug1

--- plcs.py
+++ plcs.py
@@ -1,6 +1,7 @@
 
 
-Machines_ip= dict({'Cupper1':'1','Cupper2':'xxx.xx.xx.xx',})
+Machines_ip= dict({'Cupper1':'1','Cupper2':'xxx.xx.xx.xx','Cupper3':'xx.xx.xx.xx','ss11':'xx.xx.xx.xx','ss12':'xx.xx.xx.xx','ss13':'xx.xx.xx.xx','ss14':'xx.xx.xx.xx','ss15':'xx.xx.xx.xx','ss16':'xx.xx.xx.xx','ss21':'xx.xx.xx.xx','ss22':'xx.xx.xx.xx','ss23':'xx.xx.xx.xx','ss24':'xx.xx.xx.xx','ss25':'xx.xx.xx.xx','ss26':'xx.xx.xx.xx','ss31':'xx.xx.xx.xx','ss32':'xx.xx.xx.xx','ss33':'xx.xx.xx.xx','ss34':'xx.xx.xx.xx','ss35':'xx.xx.xx.xx','ss36':'xx.xx.xx.xx'})
+
 
 def search_plc_ips():
     answers=input('search by machine name ex:Cupper1\n')
