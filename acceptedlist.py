f13aa1b9575ea169ace7bb581739908af55cc80f
jeffreyreagan <116695597+jeffreyreagan@users.noreply.github.com>
2024-01-15 22:57:30 -0500

    Update acceptedlist.py

--- acceptedlist.py
+++ acceptedlist.py
@@ -1,5 +1,4 @@
 acceptedlist = ['yes','yeah','ya','yea','Yes','YES','YA','Y','y','why not','you bet']
 rejectedlist = ['no','No','negative','N','n','nah','no thanks','not a chance']
 quitlist = ['quit','exit','EXIT','QUIT','terminate','Exit','kill','KILL','Quit','Kill','TERMINATE']
-pwdlist=['JustJeff2']
 listlist=['list','List','LIST','l','L','show','Show','SHOW','all','ALL']
